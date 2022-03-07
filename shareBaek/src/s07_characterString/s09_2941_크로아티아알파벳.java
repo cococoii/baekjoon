1b985493be2aa65c82be4a3f1c77574f979a1a5a
user <user@DESKTOP-DVT28V8>
2022-03-08 02:42:13 +0900

    dw

---
+++ shareBaek/src/s07_characterString/s09_2941_크로아티아알파벳.java
@@ -0,0 +1,27 @@
+package s07_characterString;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class s09_2941_크로아티아알파벳 {
+
+	public static void main(String[] args) throws Exception{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+		
+		String str = br.readLine();
+		int result = 0;
+		
+		for(int i=0; i<croatia.length; i++) {
+			if(str.contains(croatia[i])) {
+				str = str.replaceAll(croatia[i], "*");
+			}	
+		}
+		
+		result = str.length();
+		
+		System.out.println(result);
+	}
+}
