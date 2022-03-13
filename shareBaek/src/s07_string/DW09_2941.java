a149e4854fed91a2a77f17e6914c1ab6466eedee
user <user@DESKTOP-DVT28V8>
2022-03-13 22:35:50 +0900

    ㅇㅈ

--- shareBaek/src/s07_string/DW09_2941.java
+++ shareBaek/src/s07_string/DW09_2941.java
@@ -1,4 +1,4 @@
-package s07_characterString;
+package s07_string;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
--- shareBaek/src/s08_Math/test.java
+++
@@ -1,5 +0,0 @@
-package s08_Math;
-
-public class test {
-//test한글한글
-}
